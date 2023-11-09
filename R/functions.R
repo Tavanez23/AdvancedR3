8f0139111c902586bb8f659e1275c025f1c6ef26
Ana Rita Tavanez <anaritatavanez@gmail.com>
2023-11-09 11:14:00 +0100

    plot function as pipeline targets

--- R/functions.R
+++ R/functions.R
@@ -9,3 +9,15 @@ descriptive_stats <- function(data) {
         dplyr::summarise(across(value,list(mean = mean, sd = sd))) %>%
         dplyr::mutate(across(tidyselect::where(is.numeric), ~round(.x, digits = 1)))
 }
+
+#' ggplot function
+#'
+#' @param data
+#'
+#' @return A plot object
+plot_distributions <- function(data) {
+    data %>%
+        ggplot2::ggplot(ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
